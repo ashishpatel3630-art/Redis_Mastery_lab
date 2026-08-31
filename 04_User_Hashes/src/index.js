f43540dde1e4dc67b753ecec4542e15cbaee614c
Ashish Patel <ashishpatel3630@gmail.com>
2026-08-31 22:54:47 +0530

    hash id workinh

--- 04_User_Hashes/src/index.js
+++ 04_User_Hashes/src/index.js
@@ -2,17 +2,53 @@ import express from "express";
 import Redis from "ioredis";
 
 const app = express();
-use.app(express.json());
 
-const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+app.use(express.json());
 
-app.post("/users" , async(req , res)=>{
+const redis = new Redis(
+    process.env.REDIS_URL || "redis://localhost:6379"
+);
+
+// CREATE USER
+app.post("/users", async (req, res) => {
     const {
-        id , 
-        name ,
-        email ,
+        id,
+        name,
+        email,
         age
-    } = req.body
-    
-    await redis.hset
+    } = req.body;
+
+    await redis.hset(`user:${id}`, {
+        name,
+        email,
+        age
+    });
+
+    res.json({
+        message: "User created"
+    });
+});
+
+// GET USER
+app.get("/users/:id", async (req, res) => {
+    const user = await redis.hgetall(
+        `user:${req.params.id}`
+    );
+
+    res.json(user);
+});
+
+// DELETE USER
+app.delete("/users/:id", async (req, res) => {
+    await redis.del(`user:${req.params.id}`);
+
+    res.json({
+        message: "User deleted"
+    });
+});
+
+app.listen(3000, () => {
+    console.log(
+        "User Hashes is running on port http://localhost:3000"
+    );
 });
