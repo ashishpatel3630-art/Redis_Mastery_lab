6c8205b806d70a263668e6f5aaa8cd61c43d72fc
Ashish Patel <ashishpatel3630@gmail.com>
2026-09-01 21:23:07 +0530

    done with the counters in redis

--- 09_Counters/src/index.js
+++ 09_Counters/src/index.js
@@ -8,7 +8,7 @@ const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
 
 const COUNTERS_KEY = "COUNTERS_KEY";
 
-app.post("/vists", async(req,res)=>{
+app.post("/visits", async(req,res)=>{
     const today = new Date().toISOString().split("T")[0];
     await redis.hincrby(
         COUNTERS_KEY,
