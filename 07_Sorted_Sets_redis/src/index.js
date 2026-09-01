a250ce6f5cd0ff33c875f837131a5ca62ce8495a
Ashish Patel <ashishpatel3630@gmail.com>
2026-09-01 21:01:37 +0530

    sorted sets in redis is done

--- 07_Sorted_Sets_redis/src/index.js
+++ 07_Sorted_Sets_redis/src/index.js
@@ -22,3 +22,45 @@ app.post("/score", async(req, res)=>{
         message: "Score added"
     })
 })
+
+app.post("/score/increase" , async(req , res)=>{
+    const {
+        player ,
+        score
+    } = req.body
+    await redis.zincrby(
+        LEADERBOARD_KEY,
+        score,
+        player 
+    ); 
+    res.json({
+        message: "Score updated" ,
+        player,
+        score
+    })
+
+});
+
+app.get("/leaderboard" , async(req , res)=>{
+    const leaderboard = await redis.zrevrange(
+        LEADERBOARD_KEY,
+        0,
+        9,
+        "WITHSCORES"
+    );
+    const result =[];
+    for(let i=0 ; i<leaderboard.length ; i+=2){
+        result.push({
+            player: leaderboard[i],
+            score: leaderboard[i+1]
+        })  
+    }
+    res.json({
+        message: "Leaderboard fetched",
+        leaderboard: result
+    })  
+});
+
+app.listen(3000 , ()=>{
+    console.log("server is running on port http://localhost:3000");
+});
